2183ab49701a7e8865e7c8a344647392abcba644
Sibonelo <princesikhosana2005@gmail.com>
2026-02-19 19:33:05 +0200

    Updated gitignore and removed Notes

---
+++ Sibonelo2.0/RCode/ClassExercise19Feb.R
@@ -0,0 +1,9 @@
+# Sample of distances covered by 11 players who played atleast 5 matches in a football
+# league
+
+dist = c(58.2, 59, 57.1, 42, 32.4, 49.1, 37.4, 45.1, 36.9, 25.1, 15.5)
+
+meanDist = mean(dist)
+
+B = 1000
+
