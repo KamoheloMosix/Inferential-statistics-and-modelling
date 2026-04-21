2e9a34e8117347246339fded0ca9aa4c5e5a87fc
Sibonelo <princesikhosana2005@gmail.com>
2026-04-21 11:17:40 +0200

    Final submission REady

--- Sibonelo2.0/RCode/machineLearningInR/Tut7SKHSIB022.R
+++ Sibonelo2.0/RCode/machineLearningInR/Tut7SKHSIB022.R
@@ -41,6 +41,9 @@ PolyCV <- function(data, folds, polyNom, is_median = F){
   ifelse(is_median, median(MSE), mean(MSE))
 }
 
+# Question 1 --------------------------------------------------------------
+
+
 # WE may now begin conduction of the test
 
 TutData <- read.csv("data/TutWk7Data.csv")
@@ -54,12 +57,48 @@ polynomials <- c(1:5)
 sets <- Kfoldsplit(n_observations, 5)
 MSE_ALL_K <- foreach(polynomial = polynomials, .combine = c) %do% {
   
-  PolyCV(data = TutData, folds = sets, poly = polynomial )
+  PolyCV(data = TutData, folds = sets, polyNom = polynomial )
   
   
 }
 
-plot(seq(5), MSE_ALL_K)
-lines(seq(5), MSE_ALL_K)
+plot(seq(5), MSE_ALL_K, type = "b", pch = 19, col = "steelblue", xlab = "Polynomial Degree", ylab = "Cross-Validated MSE", main = "5-Fold CV Error vs Model Complexity", xaxt = "n")
+
+axis(1, at = 1:5, labels = 1:5)
+# vertical line highlighting the best model
+abline(v = which.min(MSE_ALL_K), lty = 2, col = "red")
 
 
+# Question 2 --------------------------------------------------------------
+
+
+
+worstModel <-lm(Y ~ poly(X,1, raw =TRUE), data = TutData)
+
+bestModel <- lm(Y ~ poly(X,3, raw =TRUE), data = TutData)
+
+# data i will use for the predictions
+feats <- data.frame(X = TutData$X)
+
+# Worst model, with degree 1 -- Too simple
+predWorst <- predict(worstModel, feats)
+
+errorWorst <- mean( (TutData$Y - predWorst)^2)
+
+
+# Best model, with degree 3 -- balanced
+predBest <- predict(bestModel, feats)
+
+errorBest <- mean( (TutData$Y - predBest)^2)
+
+# 2.2
+x_grid <- seq(min(TutData$X), max(TutData$X), length=500)
+new_data <- data.frame(X = x_grid)  
+
+bestLine <- predict(bestModel, newdata = new_data)
+worstLine <- predict(worstModel, newdata = new_data)
+
+plot(TutData$X, TutData$Y, pch  = 19, col = "grey60", xlab = "X", ylab = "Y", main = "Degree 1 vs Degree 3 Polynomial Fit")
+
+lines(x_grid, bestLine, col = 'blue', lwd = 2)
+lines(x_grid, worstLine, col = 'red', lwd =2)
